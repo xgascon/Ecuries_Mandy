39fd83f1cbd7f1d1d05f0c3284704202d0865559
xgascon <65670213+xgascon@users.noreply.github.com>
2021-02-13 09:19:22 +0100

    Pages : MentionsLég + Stages, jquery advanced with logo apparition in Index, less animation tarifs page

--- index.js
+++ index.js
@@ -10,22 +10,12 @@ $(function () {
             $('.navbar').addClass('navbar-light');
         }
         apparitionWhenVisibleAllAnimations();
-        // apparitionWhenVisible('.testscroll1', 'apparition-scale');
-        // if (($(window).height()+$(window).scrollTop())>$('.testscroll2').offset().top) {
-        //     $('.testscroll2').css( "visibility", "visible" );
-        //     $('.testscroll2').addClass('apparition-scale')
-        // }
     });
 });
 
-// $(function () {
-    // var charts = $('.chart');
-
 function apparitionWhenVisible (element, animation) {
     var elements = $(element);
-    // console.log(elements.length);
     for (var i = 0, count = elements.length; i < count; i++) {
-        // console.log(elements[i])
         if (($(window).height()+$(window).scrollTop())>$(elements[i]).offset().top) {
             $(elements[i]).css( "visibility", "visible" );
             $(elements[i]).addClass(animation);
@@ -40,27 +30,45 @@ apparitionWhenVisible('.apparition-scale-1', 'apparition-scale-1-animation');
 apparitionWhenVisible('.apparition-scale-2', 'apparition-scale-2-animation');
 apparitionWhenVisible('.apparition-scale-3', 'apparition-scale-3-animation');
 apparitionWhenVisible('.apparition-bottom', 'apparition-bottom-animation');
+apparitionWhenVisible('.apparition-left-1', 'apparition-left-1-animation');
+apparitionWhenVisible('.apparition-left-2', 'apparition-left-2-animation');
 apparitionWhenVisible('.apparition-right-1', 'apparition-right-1-animation');
 apparitionWhenVisible('.apparition-right-2', 'apparition-right-2-animation');
+apparitionWhenVisible('.apparition-right-3', 'apparition-right-3-animation');
 apparitionWhenVisible('.apparition-bottom-1', 'apparition-bottom-1-animation');
 apparitionWhenVisible('.apparition-bottom-2', 'apparition-bottom-2-animation');
 apparitionWhenVisible('.apparition-bottom-3', 'apparition-bottom-3-animation');
-apparitionWhenVisible('.yellow-underline', 'bg-100');
+apparitionWhenVisible('.yellow-underline', 'yellow-underline--bg-size');
 }
 
 apparitionWhenVisibleAllAnimations ()
 
-    // function animateChartWhenVisible (chart) {
-    //     for (var i = 0, count = chart.length; i < count; i++) {
-    //         if (isVisible(chart[i])) {
-    //             $(chart[i]).addClass();
-    //         }
-    //     }
-    // }
+function stages__description__buttons (buttonToClick, content) {
+    document.getElementById(buttonToClick).addEventListener("click", function () {
+        let stages__description__div = document.getElementById('stages__description__div');
+        let description__text = document.getElementById('stages__description__div__text');
+        
+        stages__description__div.removeChild(description__text);
+        let newDescription__text = document.createElement('p');
+        newDescription__text.innerHTML = content;
+        newDescription__text.className = "scroll-apparition apparition-bright";
+        newDescription__text.id = "stages__description__div__text";
+    
+        stages__description__div.appendChild(newDescription__text);
+    
+        apparitionWhenVisibleAllAnimations ();
+    });
+}
 
-// });
+function animateStagesButtonsDescription (stages__button) {
+    var stages__buttons = $(stages__button);
+    for (var i = 1, count = stages__buttons.length ; i <= count; i++) {
+        stages__description__buttons('stages__button--'+i, document.getElementById('stages__description__text--'+i).innerHTML);
+    }
+}
 
-// if (($(window).height()+$(window).scrollTop())>$('.testscroll2').offset().top) {
-//     $('.testscroll2').css( "visibility", "visible" );
-//     $('.testscroll2').addClass('apparition-scale');
-// }
+animateStagesButtonsDescription('.stages__elements__ribbon');
+
+$(function () {
+    jQuery('.preloader').fadeOut(1000)
+})
